992dbcd6a2b2a2d5dac5e7ef4b4a9e4c06c6e17f
Alaafathhyy <57330162+Alaafathhyy@users.noreply.github.com>
2020-06-19 16:42:11 +0200

    Create  Dividing coins.cpp

---
+++ Dividing coins.cpp
@@ -0,0 +1,41 @@
+//E - Dividing coins UVA - 562 
+#define MOD 1000000007
+#define input ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define  ll long long
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int const N = 50000;
+int arr[105];
+int n;
+
+ll dp[105][N];
+ll alaa(int i, ll sum1) {
+
+    if (i == n) {
+        return abs(sum1);
+    }
+    ll &ret=dp[i][sum1];
+    if(~ret)
+        return ret;
+
+    return ret =min(alaa(i+1,sum1+arr[i]),alaa(i+1,sum1-arr[i]));
+}
+
+int main() {
+    int cases;
+    cin >> cases;
+
+    while (cases--) {
+    memset(dp,-1, sizeof(dp));
+        cin >> n;
+        ll sum1 = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            sum1 += arr[i];
+        }
+        cout << alaa(0, 0) << endl;
+    }
+}
